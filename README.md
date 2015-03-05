c0afa74f702d9ef7f88810dc7759a7c6e0c0310a
Kai Kramer <kai@gaulstall.de>
2015-03-05 22:58:37 +0100

    write with set

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# mqtt-knx
+## mqtt-knx
+
 KNX to MQTT Bridge
 
 Work in progress
+
+# Example
+
+
+    ./mqtt-knx.js -e smarthome -m mqtt://mac-server.local
--- mqtt-knx.js
+++ mqtt-knx.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-"use strict";
+'use strit';
 
 var eibd = require('eibd'),
   mqtt = require('mqtt'),
@@ -17,30 +17,43 @@ var argv = optimist
   .argv;
 
 
-function listenEIBD(opts, callback) {
-  var conn = eibd.Connection();
+var mqttClient = mqtt.connect(argv.mqtt);
 
-  conn.socketRemote(opts, function () {
-    conn.openGroupSocket(0, callback);
+var eibdConn = eibd.Connection();
+var eibdOpts = {host: argv.eibd, port: argv.eibdport};
+
+function groupWriteDPT1(gad, value) {
+  var address = eibd.str2addr(gad);
+  eibdConn.socketRemote(eibdOpts, function () {
+    eibdConn.openTGroup(address, 1, function (err) {
+      var msg = eibd.createMessage('write', 'DPT3', parseInt(value));
+      eibdConn.sendAPDU(msg, function (err) {
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
   });
 }
 
+mqttClient.subscribe(argv.topic + '/+/+/+/set');
 
-var client = mqtt.connect(argv.mqtt, function (parser) {
-  parser.on('error', function (error) {
-    error.log(error);
-  });
+mqttClient.on('message', function (topic, message) {
+  var gad = topic.substr(argv.topic.length + 1, topic.length - argv.topic.length - 5);
+  console.log('mqttClient.on', gad, message.toString());
+  groupWriteDPT1(gad, message.toString());
 });
 
-client.subscribe(argv.topic + '/+/+/+/set');
-
-client.on('message', function (topic, message) {
-  if (message == 'true') {
-
-  } else if (message == 'true') {
-
-  }
-  //console.log(topic, message.toString());
+eibdConn.socketRemote(eibdOpts, function () {
+  eibdConn.openGroupSocket(0, function (parser) {
+    parser.on('write', function (src, dest, type, val) {
+      var message = getDPTValue(val, type);
+      if (message) {
+        console.log(argv.topic + '/' + dest, message);
+        mqttClient.publish(argv.topic + '/' + dest, message, {retain: argv.retain});
+      }
+    });
+  });
 });
 
 function getDPTValue(val, type) {
@@ -60,17 +73,4 @@ function getDPTValue(val, type) {
       return undefined;
   }
 }
-
-listenEIBD({host: argv.eibd, port: argv.eibdport}, function (parser) {
-
-  parser.on('write', function (src, dest, type, val) {
-    console.log('Write from ' + src + ' to ' + dest + ': ' + val + ' [' + type + ']');
-    var message = getDPTValue(val, type);
-    if (message) {
-      console.log(argv.topic + '/' + dest, message);
-      client.publish(argv.topic + '/' + dest, message, {retain: argv.retain});
-    }
-  });
-
-});
 

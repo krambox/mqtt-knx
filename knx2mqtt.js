1776ce65aefc9b90df27a568de6764fb905cd6a3
Kai Kramer <kai@gaulstall.de>
2016-01-31 17:23:19 +0100

    update format, remove ide

--- knx2mqtt.js
+++ knx2mqtt.js
@@ -1,48 +1,39 @@
 #!/usr/bin/env node
 
 var eibd = require('eibd');
-var mqtt    = require('mqtt');
+var mqtt = require('mqtt');
+var yaml_config = require('node-yaml-config');
 
- 
+var config = yaml_config.load(__dirname + '/config.yml');
 
 /**
  * groupsocketlisten
  */
 function groupsocketlisten(opts, callback) {
-
-  var conn = eibd.Connection();
-
-  conn.socketRemote(opts, function() {
-    
-    conn.openGroupSocket(0, callback);
-
-  });
-
+    var conn = eibd.Connection();
+    conn.socketRemote(opts, function () {
+        conn.openGroupSocket(0, callback);
+    });
 }
 
-var host = '192.168.1.100'
-var port = '6720'
+var host = config.eibdHost;
+var port = config.eibdPort;
 
-var client  = mqtt.connect('mqtt://localhost');
+var client = mqtt.connect(config.mqttHost);
 
-groupsocketlisten({ host: host, port: port }, function(parser) {
+groupsocketlisten({ host: host, port: port }, function (parser) {
+    parser.on('write', function (src, dest, type, val) {
+        console.log('Write from ' + src + ' to ' + dest + ': ' + val + ' [' + type + ']');
+        client.publish('eibd/' + dest, '' + val, { retain: true });
+    });
 
-  parser.on('write', function(src, dest, type, val){
-    console.log('Write from '+src+' to '+dest+': '+val+' ['+type+']');
-    
-    client.publish('eibd/'+dest, ''+val, {retain: true});
-  });
+    parser.on('response', function (src, dest, type, val) {
+        console.log('Response from ' + src + ' to ' + dest + ': ' + val + ' [' + type + ']');
+        //client.publish('eibd/'+dest, 'Response from src: '+src+': '+val+' ['+type+']', {retain: true});
+    });
 
-  parser.on('response', function(src, dest, type, val) {
-    console.log('Response from '+src+' to '+dest+': '+val+' ['+type+']');
-    //client.publish('eibd/'+dest, 'Response from src: '+src+': '+val+' ['+type+']', {retain: true});
-
-  });
-  
-  parser.on('read', function(src, dest) {
-    console.log('Read from '+src+' to '+dest);
-    //client.publish('eibd/'+dest, 'Read from src: '+src, {retain: true});
-
-  });
-
+    parser.on('read', function (src, dest) {
+        console.log('Read from ' + src + ' to ' + dest);
+        //client.publish('eibd/'+dest, 'Read from src: '+src, {retain: true});
+    });
 });
